dbbc1b5db520327f8c74fd4c43bc2a5262a286aa
klasshammer <cmaureiravargas@icloud.com>
2026-04-21 14:10:49 -0400

    Personalizacion EC2 1.1

--- EA2/ACT2.1/main.tf
+++ EA2/ACT2.1/main.tf
@@ -1,3 +1,10 @@
+/*
+Personalización del Módulo EC2
+1.- Una instancia en una subred pública distinta a la ya configurada.
+*/
+
+
+
 module "vpc" {
   source                = "./vpc_module"
   vpc_cidr              = "10.1.0.0/16"
@@ -16,6 +23,18 @@ module "ec2" {
   public_key    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDiuFUssdtHg8Y3rWGZFCSD58hSr4IqjFVKeid9d0G3bk7w99/AOyL/C45PnFodjOtD1eMndiCd40BqagdOYtKoieqlOTlmShrvE7N2A+MeaOP4CWLx7fj2MfekecPPFRAiMUCZk51SHxFr4oqX4Qhj8BkG1cG30p9QB+stfJKT3tUGczxUB1aor9qoLmPDTfaE4iSmNDscVmqQhX9jkppdzkg2ENh5cDO2EtLlHHxIodXLgetpWjBP68r90q/gwZV69XANcTWjZiZRyDmb9nIfQiZOO5C03FoG0GmTSZkAfvZdq7M2GsQSboln44VW/ukyQKFRVVepOCIHTaqcsjhV"
   ami           = "ami-012967cc5a8c9f891"
   subnet_id     = module.vpc.subnet_publica_1_id
+  vpc_id        = module.vpc.vpc_id
+  instance_name = "MiInstancia"
+}
+
+// Repetimos el codigo pero aseguramos que tenga otro nombre
+module "ec2_2" {
+  source        = "./ec2_module"
+  key_name      = "mi_key_name"
+  public_key    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDiuFUssdtHg8Y3rWGZFCSD58hSr4IqjFVKeid9d0G3bk7w99/AOyL/C45PnFodjOtD1eMndiCd40BqagdOYtKoieqlOTlmShrvE7N2A+MeaOP4CWLx7fj2MfekecPPFRAiMUCZk51SHxFr4oqX4Qhj8BkG1cG30p9QB+stfJKT3tUGczxUB1aor9qoLmPDTfaE4iSmNDscVmqQhX9jkppdzkg2ENh5cDO2EtLlHHxIodXLgetpWjBP68r90q/gwZV69XANcTWjZiZRyDmb9nIfQiZOO5C03FoG0GmTSZkAfvZdq7M2GsQSboln44VW/ukyQKFRVVepOCIHTaqcsjhV"
+  ami           = "ami-012967cc5a8c9f891"
+  //subnet_id     = module.vpc.subnet_publica_1_id
+  subnet_id     = module.vpc.subnet_publica_2_id
   vpc_id        = module.vpc.vpc_id
   instance_name = "MiInstancia"
 }
